5351d91172d6fa1f51e994d65b8164d74d9ad5fe
Teodor <ursus_24@yahoo.com>
2022-09-24 15:16:11 +0300

    add products and other pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,7 @@
 const date = document.getElementById("footer-h1");
 date.innerText += " " + new Date().getFullYear() + " " + "Briceanu Teodor";
+let togglecls = document.querySelectorAll(".navbar")[0];
+let togglebtn = document.querySelectorAll(".hamburger-menu")[0];
+togglebtn.addEventListener("click", function (e) {
+  togglecls.classList.toggle("toggle");
+});
